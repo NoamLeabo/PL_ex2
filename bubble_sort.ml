29542864db3f1ed8af6cb05c998930f5819d84a5
Noam Leabo <155389867+NoamLeabo@users.noreply.github.com>
2024-12-18 01:35:22 +0200

    Update bubble_sort.ml

--- bubble_sort.ml
+++ bubble_sort.ml
@@ -3,57 +3,41 @@ open Nos
 open Semantics
 
 let run_bubble_sort n =
-  (* Generate variable names "X1", "X2", ..., "Xn" *)
+  (* Generate variable names "x1", "x2", ..., "xn" *)
   let var_name i = "x" ^ string_of_int i in
 
-  (* Initialize variables X1 = n, X2 = n-1, ..., Xn = 1 *)
+  (* Initialize variables x1 = n, x2 = n-1, ..., xn = 1 *)
   let rec init_vars i =
     if i = 0 then Skip
-    else Comp (Ass (var_name i, Num i), init_vars (i - 1))
+    else Comp (Ass (var_name i, Num (n + 1 - i)), init_vars (i - 1))
   in
 
-  (* Generate the swap logic for two variables xi and xj *)
+  (* Swap logic for two variables xi and xj *)
   let swap_logic xi xj =
-    Comp (Ass ("temp", Var xi),                   (* temp = xi *)
-    Comp (Ass (xi, Var xj),                      (* xi = xj *)
-          Ass (xj, Var "temp")))                 (* xj = temp *)
+    Comp (Ass ("temp", Var xi),       (* temp = xi *)
+    Comp (Ass (xi, Var xj),           (* xi = xj *)
+          Ass (xj, Var "temp")))      (* xj = temp *)
   in
 
-  (* Inner loop: while j <= n, compare and swap xi and xj if needed *)
-  let inner_loop i =
-    While (Neg (Gte (Var "j", Num (n + 1))),     (* while j < n+1 *)
+  (* Generate all pairwise comparisons and swaps (n^2 if-statements) *)
+  let rec generate_if_statements i j =
+    if i > n then Skip
+    else if j > n then generate_if_statements (i + 1) (i + 1)  (* Move to next i, reset j *)
+    else if i >= j then generate_if_statements i (j + 1)       (* Skip if i >= j *)
+    else
+      (* Compare xi and xj and swap if xi > xj *)
       Comp (
-        If (Gte (Var (var_name i), Var ("x" ^ string_of_int i)),  (* if xi >= xj *)
-            swap_logic (var_name i) ("x" ^ string_of_int i),     (* swap xi, xj *)
-            Skip                                      (* else: do nothing *)
+        If (Gte (Var (var_name i), Var (var_name j)),
+            swap_logic (var_name i) (var_name j),
+            Skip
         ),
-        Ass ("j", Add (Var "j", Num 1))          (* j = j + 1 *)
+        generate_if_statements i (j + 1)
       )
-    )
   in
 
-  (* Outer loop: while i < n, iterate through elements *)
-  let rec outer_loop i =
-    If (Gte (Num i, Num n),                      (* if i >= n *)
-        Skip,                                    (* then: do nothing *)
-        Comp (                                   (* else: run the bubble sort logic *)
-          Ass ("i", Num i),                      (* i = current iteration *)
-          While (Neg (Gte (Var "i", Num n)),     (* while i < n *)
-            Comp (
-              Ass ("j", Add (Var "i", Num 1)),   (* j = i + 1 *)
-              Comp (
-                inner_loop i,                    (* Perform inner loop *)
-                Ass ("i", Add (Var "i", Num 1))  (* i = i + 1 *)
-              )
-            )
-          )
-        )
-    )
-  in
-
-  (* Combine everything: initialization and the bubble sort logic *)
+  (* Combine initialization and explicit pairwise comparisons *)
   Comp (
-    init_vars n,         (* Initialize the variables *)
-    outer_loop 1         (* Run the bubble sort algorithm *)
+    init_vars n,                    (* Initialize variables *)
+    generate_if_statements 1 2      (* Generate all if-statements for i, j *)
   )
 ;;
